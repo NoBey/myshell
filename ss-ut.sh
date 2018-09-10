c403dce762d70994ce1e0e1542acedfe2de3ab8a
NoBey <nobey@nobey.cn>
2018-09-10 18:09:17 +0800

    update

--- ss-ut.sh
+++ ss-ut.sh
@@ -1,6 +1,6 @@
 apt-get update && 
-python3 setuptools/setup.py install &&
-python3 pip/setup.py install &&
+python setuptools/setup.py install &&
+python pip/setup.py install &&
 pip install shadowsocks &&
 ssserver -c ss.json -d start
 
