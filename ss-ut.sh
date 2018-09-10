071f12a2149b19b25aa12eb9ebf515e361d9800f
NoBey <nobey@nobey.cn>
2018-09-10 17:23:45 +0800

    init

---
+++ ss-ut.sh
@@ -0,0 +1,4 @@
+apt-get install python-pip -y
+apt-get install python-m2crypto -y
+pip install shadowsocks
+ssserver -c ss.json -d start
---
+++ zsh.sh
@@ -0,0 +1,3 @@
+apt install zsh -y 
+yum install zsh -y
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
