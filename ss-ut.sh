68876a85bcfb3b57c78194199007f562e6ff9be7
NoBey <nobey@nobey.cn>
2018-09-10 17:40:04 +0800

    update

--- ss-ut.sh
+++ ss-ut.sh
@@ -1,4 +1,4 @@
-apt-get update
-apt-get install python-pip python-m2crypto -y
-pip install shadowsocks
+apt-get update && 
+apt-get install python-pip python-m2crypto -y &&
+pip install shadowsocks &&
 ssserver -c ss.json -d start
